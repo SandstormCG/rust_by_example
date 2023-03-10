85895e46dc40b4a304bf47843262544f2ba2e96b
sandstorm <git@sandstorm.mozmail.com>
2023-03-10 11:43:00 +0100

    1. Hello world solved FIXME

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 pub mod hello_world; 
 
 fn main() {
+    first_chapter();
+}
+
+
+fn first_chapter() {
     println!("============================================================================");
     println!("========================= 1. Hello World ===================================");
     println!("============================================================================");
     hello_world::hello_rustaceans();
+    println!("****1.1. Formatted print******");
+    hello_world::formatted_print();
 }
