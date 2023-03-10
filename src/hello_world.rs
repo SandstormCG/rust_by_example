f9a68adc6de3fd63780126792192b300b5ba8865
sandstorm <git@sandstorm.mozmail.com>
2023-03-10 11:43:00 +0100

    Finished 1st chapter - Hello world

--- src/hello_world.rs
+++ src/hello_world.rs
@@ -1,4 +1,7 @@
 use std::fmt;
+//use std::fmt::{self, Formatter, Display};
+use std::fmt::{Formatter, Display};
+
 
 // This is a comment, and is ignored by the compiler
 // You can test this code by clicking the "Run" button over there ->
@@ -106,8 +109,8 @@ pub fn formatted_print() {
     let width: usize = 5;
     println!("{number:>width$}");
 
-
-    //Pi is roughly 3.142
+    let pi = 3.141592;
+    println!("Pi is roughly {:.3}", pi);
 }
 
 pub fn debug_example() {
@@ -232,5 +235,54 @@ pub fn display_example() {
 
     let v = List(vec![1, 2, 3]);
     println!("{}", v);
+
+}
+
+pub fn format_example() {
+
+    struct City {
+        name: &'static str,
+        // Latitude
+        lat: f32,
+        // Longitude
+        lon: f32,
+    }
+
+    impl Display for City {
+        // `f` is a buffer, and this method must write the formatted string into it
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+            let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+            // `write!` is like `format!`, but it will write the formatted string
+            // into a buffer (the first argument)
+            write!(f, "{}: {:.3}°{} {:.3}°{}",
+                self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+        }
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{:?}", *color);
+    }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,13 @@ fn first_chapter() {
     /*println!("============================================================================");
     println!("========================= 1. Hello World ===================================");
     println!("============================================================================");
-    hello_world::hello_rustaceans();
+    hello_world::hello_rustaceans();*/
     println!("****1.2. Formatted print******");
     hello_world::formatted_print();
-    println!("****1.2.1 Debug******");
-    hello_world::debug_example();*/
+    /*println!("****1.2.1 Debug******");
+    hello_world::debug_example();
     println!("****1.2.2 Display******");
     hello_world::display_example();
+    println!("****1.2.3 Format******");
+    hello_world::format_example();*/
 }
