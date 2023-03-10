5c885bb5de8aefd78959fae07a8e8ccc73b62a60
sandstorm <git@sandstorm.mozmail.com>
2023-03-10 11:43:00 +0100

    1. Hello World

---
+++ src/hello_world.rs
@@ -0,0 +1,33 @@
+// This is a comment, and is ignored by the compiler
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function
+pub fn hello_rustaceans() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
+
+
+// Line comments which go to the end of the line.
+
+
+/* 
+* This is another type of comment, a block comment. In general,
+* line comments are the recommended comment style. But
+* block comments are extremely useful for temporarily disabling
+* chunks of code. /* Block comments can be /* nested, */ */
+* so it takes only a few keystrokes to comment out everything
+* in this main() function. /*/*/* Try it yourself! */*/*/
+*/
+
+/*
+Note: The previous column of `*` was entirely for style. There's
+no actual need for it.
+*/
